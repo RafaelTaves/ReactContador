d94d47676088e443612362b45b2bdf7a43ba3a9e
RafaelTaves <rafaeljosetaves@gmail.com>
2024-05-17 17:23:55 -0300

    Atualização de estilo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
-import { useState } from "react";
-import BtnAdicionar from "./componentes/BtnAdicionar";
-import Visor from "./componentes/visor";
-import BtnSubtrair from "./componentes/BtnSubtrair";
+import Contador from "./componentes/contador";
 
 function App() {
-  const [valorInicial, setValorInicial] = useState(0)
-
-  const incrementarValor = () => {
-    setValorInicial(valorInicial + 1)
-  }
-
-  const subtrairValor = () => {
-    setValorInicial(valorInicial - 1)
-  }
-
+  
   return (
 
     <div className="App">
-      <Visor valor={valorInicial} />
-      <BtnAdicionar aoAlterar={incrementarValor} />
-      <BtnSubtrair aoAlterar={subtrairValor} />
+      <Contador/>
     </div>
   );
 }
--- src/componentes/contador/index.js
+++ src/componentes/contador/index.js
@@ -1,15 +1,24 @@
+import { useState } from 'react'
 import BtnAdicionar from '../BtnAdicionar'
 import BtnSubtrair from '../BtnSubtrair'
 import Visor from '../visor'
 import './Contador.css'
 
 const Contador = () => {
-    
+    const [valorInicial, setValorInicial] = useState(0)
+
+    const incrementarValor = () => {
+    setValorInicial(valorInicial + 1)
+    }
+
+    const subtrairValor = () => {
+    setValorInicial(valorInicial - 1)
+    }
     return(
         <section className='container'>
-            <BtnSubtrair/>
-            <Visor />
-            <BtnAdicionar />
+            <BtnSubtrair aoAlterar={subtrairValor} />
+            <Visor valor={valorInicial} />
+            <BtnAdicionar aoAlterar={incrementarValor} />
         </section>
     )
 }
